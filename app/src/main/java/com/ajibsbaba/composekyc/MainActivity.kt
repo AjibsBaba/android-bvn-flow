822e986303b44a228d6bc6b1fa20ead75ecf8032
Samuel Ajibade <samuelajibade22@gmail.com>
2024-01-06 02:12:38 +0100

    initial commit

---
+++ app/src/main/java/com/ajibsbaba/composekyc/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.ajibsbaba.composekyc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.ajibsbaba.composekyc.ui.theme.ComposeKYCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeKYCTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
