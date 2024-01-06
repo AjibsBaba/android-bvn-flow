cc17366b26bdcacffb0a08d5248871f0345cf8be
Samuel Ajibade <samuelajibade22@gmail.com>
2024-01-06 02:49:34 +0100

    refactored ui into components

--- app/src/main/java/com/ajibsbaba/composekyc/ui/theme/Color.kt
+++ app/src/main/java/com/ajibsbaba/composekyc/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package com.ajibsbaba.composekyc.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple = Color(0xFF796CF7)
+val Blue30 = Color(0xFF66B3E0)
+val Blue40 = Color(0xFF40A0D8)
+val DarkGrey = Color(0xFFA4ACB4)
